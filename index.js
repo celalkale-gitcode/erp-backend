262b8ca04ec87c64013f07b9e6c1231d05ca3c94
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 00:23:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 catch (err) {
   console.error("FULL ERROR:", err);
-  res.status(500).json({
-    message: err.message,
-    code: err.code,
-    meta: err.meta
-  });
+  res.status(500).json(err.message);
 }
