ad6a6a0a99c1e814d56cea02d91387af7328bff2
celalkale-gitcode <celalkale@gmail.com>
2026-04-25 20:21:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API çalışıyor 🚀");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server started");
+});
