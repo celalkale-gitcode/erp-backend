549731bf627e530ba7ee2a25b04fd53ce6d7f367
celalkale-gitcode <celalkale@gmail.com>
2026-04-25 18:34:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API çalışıyor 🚀");
+});
+
+app.post("/count", async (req, res) => {
+  const { urun_id, latitude, longitude } = req.body;
+
+  const data = await prisma.stok_hareketleri.create({
+    data: {
+      urun_id,
+      tip: "COUNT",
+      miktar: 1,
+      latitude,
+      longitude
+    }
+  });
+
+  res.json(data);
+});
+
+app.get("/map", async (req, res) => {
+  const data = await prisma.stok_hareketleri.findMany();
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000);
