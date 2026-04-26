3a12515422a4f0005ee01fe3f3d53a5616c2621b
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 14:11:59 +0300

    Update inventory.conroller.ts

--- backend/src/modules/inventory/inventory.conroller.ts
+++ backend/src/modules/inventory/inventory.conroller.ts
@@ -1,1 +1,15 @@
 
+import { Controller, Post, Body } from '@nestjs/common';
+import { InventoryService } from './inventory.service';
+
+@Controller('inventory')
+export class InventoryController {
+  constructor(private readonly service: InventoryService) {}
+
+  @Post('count')
+  async count(@Body() body: any) {
+    const { urun_id, latitude, longitude } = body;
+
+    return this.service.count(urun_id, latitude, longitude);
+  }
+}
