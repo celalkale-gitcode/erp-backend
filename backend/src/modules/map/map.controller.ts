2a187f60d720a5b2b7e2dc05b2af5d34c232ef66
celalkale-gitcode <celalkale@gmail.com>
2026-04-27 00:06:15 +0300

    Implement MapController with getAll endpoint

--- backend/src/modules/map/map.controller.ts
+++ backend/src/modules/map/map.controller.ts
@@ -1,1 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { MapService } from './map.service';
 
+@Controller('map')
+export class MapController {
+  constructor(private readonly service: MapService) {}
+
+  @Get()
+  getAll() {
+    return this.service.getAllPoints();
+  }
+}
