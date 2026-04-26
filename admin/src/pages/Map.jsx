6bdcde102743661b84c8b8a54efe5737627eef34
celalkale-gitcode <celalkale@gmail.com>
2026-04-27 00:29:33 +0300

    Fetch and display map data from API

--- admin/src/pages/Map.jsx
+++ admin/src/pages/Map.jsx
@@ -1,1 +1,21 @@
+import { useEffect, useState } from "react";
+import API from "../api";
 
+export default function Map() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    API.get("/map").then(res => setData(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Harita Verisi</h3>
+      {data.map(i => (
+        <div key={i.id}>
+          {i.latitude} - {i.longitude}
+        </div>
+      ))}
+    </div>
+  );
+}
