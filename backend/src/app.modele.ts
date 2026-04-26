76a3b4e5a76f775fc353c8d04a6543c8a7792a1f
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 23:29:27 +0300

    Reorganize module imports in AppModule

--- backend/src/app.modele.ts
+++ backend/src/app.modele.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './modules/product/product.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
-import { BarcodeModule } from './modules/barcode/barcode.module';
-import { LocationModule } from './modules/location/location.module';
 import { ReturnsModule } from './modules/returns/returns.module';
-import { MapModule } from './modules/map/map.module';
-import { SyncModule } from './modules/sync/sync.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
   imports: [
-    ProductModule,
     InventoryModule,
-    BarcodeModule,
-    LocationModule,
     ReturnsModule,
-    MapModule,
-    SyncModule
+    ProductModule
   ]
 })
 export class AppModule {}
