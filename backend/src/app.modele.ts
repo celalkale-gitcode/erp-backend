29ba93df62c75bc9de5ae6863f831d23aa89f5e0
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 13:55:03 +0300

    Update app.modele.ts

--- backend/src/app.modele.ts
+++ backend/src/app.modele.ts
@@ -1,1 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ProductModule } from './modules/product/product.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { BarcodeModule } from './modules/barcode/barcode.module';
+import { LocationModule } from './modules/location/location.module';
+import { ReturnsModule } from './modules/returns/returns.module';
+import { MapModule } from './modules/map/map.module';
+import { SyncModule } from './modules/sync/sync.module';
 
+@Module({
+  imports: [
+    ProductModule,
+    InventoryModule,
+    BarcodeModule,
+    LocationModule,
+    ReturnsModule,
+    MapModule,
+    SyncModule
+  ]
+})
+export class AppModule {}
