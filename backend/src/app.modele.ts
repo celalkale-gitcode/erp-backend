aff1d08edf10839a75238ffc09a0a3d12f9fdda7
celalkale-gitcode <celalkale@gmail.com>
2026-04-27 00:11:23 +0300

    Add Location, Map, and Sync modules to AppModule

--- backend/src/app.modele.ts
+++ backend/src/app.modele.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { ReturnsModule } from './modules/returns/returns.module';
 import { ProductModule } from './modules/product/product.module';
+import { LocationModule } from './modules/location/location.module';
+import { MapModule } from './modules/map/map.module';
+import { SyncModule } from './modules/sync/sync.module';
 
 @Module({
   imports: [
     InventoryModule,
     ReturnsModule,
-    ProductModule
+    ProductModule,
+    LocationModule,
+    MapModule,
+    SyncModule
   ]
 })
 export class AppModule {}
