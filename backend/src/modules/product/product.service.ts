4b65931827910e3e79a79630c7326c0801510ca2
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 13:31:24 +0300

    Update product.service.ts

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,1 +1,12 @@
+async create(dto) {
+  const urun = await this.prisma.urunler.create({ data: dto });
 
+  const base = "999" + urun.id.toString().padStart(9, '0');
+  const code = this.barcode.generateEAN13(base);
+
+  await this.prisma.urunBarkodlari.create({
+    data: { urunId: urun.id, sistemBarkod: code }
+  });
+
+  return urun;
+}
