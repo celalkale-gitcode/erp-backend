42c47a94a6df00dca645c7a78cbf59bf0bd13f09
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 14:06:32 +0300

    Update product.controller.ts

--- backend/src/modules/product/product.controller.ts
+++ backend/src/modules/product/product.controller.ts
@@ -1,1 +1,17 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
+@Controller('products')
+export class ProductController {
+  constructor(private service: ProductService) {}
+
+  @Post()
+  create(@Body() body) {
+    return this.service.create(body);
+  }
+
+  @Get(':barkod')
+  getByBarcode(@Param('barkod') barkod: string) {
+    return this.service.findByBarcode(barkod);
+  }
+}
