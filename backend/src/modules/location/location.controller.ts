4f5173b5b8f382feee4ddb879cd6866d684e3b9e
celalkale-gitcode <celalkale@gmail.com>
2026-04-27 00:02:49 +0300

    Implement LocationController with CRUD operations

--- backend/src/modules/location/location.controller.ts
+++ backend/src/modules/location/location.controller.ts
@@ -1,1 +1,26 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { LocationService } from './location.service';
 
+@Controller('location')
+export class LocationController {
+  constructor(private readonly service: LocationService) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.service.createLocation(body.raf_kodu);
+  }
+
+  @Post('assign')
+  assign(@Body() body: any) {
+    return this.service.assignProduct(
+      body.lokasyon_id,
+      body.urun_id,
+      body.beden
+    );
+  }
+
+  @Get(':id')
+  get(@Param('id') id: string) {
+    return this.service.getLocationProducts(Number(id));
+  }
+}
