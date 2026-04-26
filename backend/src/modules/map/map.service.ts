6b37c8c25d24bd8e5ab4c4d8988b2f513cc7310f
celalkale-gitcode <celalkale@gmail.com>
2026-04-27 00:04:53 +0300

    Update map.service.ts

--- backend/src/modules/map/map.service.ts
+++ backend/src/modules/map/map.service.ts
@@ -1,1 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { prisma } from '../../common/prisma.service';
 
+@Injectable()
+export class MapService {
+  async getAllPoints() {
+    return prisma.stok_hareketleri.findMany({
+      where: {
+        latitude: { not: null },
+        longitude: { not: null }
+      }
+    });
+  }
+}
