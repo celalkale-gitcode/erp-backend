2a7811a3bbbf6f4c16cfebec9dde8382c7257c30
celalkale-gitcode <celalkale@gmail.com>
2026-04-26 14:04:12 +0300

    Update product.service.ts

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,12 +1,11 @@
-async create(dto) {
-  const urun = await this.prisma.urunler.create({ data: dto });
+import { prisma } from '../../common/prisma.service';
 
-  const base = "999" + urun.id.toString().padStart(9, '0');
-  const code = this.barcode.generateEAN13(base);
+export class ProductService {
+  async create(data) {
+    return prisma.urunler.create({ data });
+  }
 
-  await this.prisma.urunBarkodlari.create({
-    data: { urunId: urun.id, sistemBarkod: code }
-  });
-
-  return urun;
+  async findByBarcode(barkod: string) {
+    return prisma.urunler.findFirst({ where: { barkod } });
+  }
 }
