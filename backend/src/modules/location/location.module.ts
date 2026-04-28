62434f32995420691110e51e68255e004c9e5ca0
celalkale-gitcode <celalkale@gmail.com>
2026-04-28 14:55:10 +0300

    Add PrismaService to LocationModule providers

--- backend/src/modules/location/location.module.ts
+++ backend/src/modules/location/location.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
+import { LocationController } from './location.controller';
+import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [LocationController],
-  providers: [LocationService]
+  providers: [LocationService, PrismaService],
 })
 export class LocationModule {}
