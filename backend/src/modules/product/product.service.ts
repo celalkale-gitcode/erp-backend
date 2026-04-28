cd7ff12b8c245716bb8b4a27405e33cc55b95073
celalkale-gitcode <celalkale@gmail.com>
2026-04-29 00:59:55 +0300

    Update product.service.ts

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -3,15 +3,19 @@ import { PrismaService } from '../../common/prisma.service';
 
 @Injectable()
 export class ProductService {
-  constructor(private prisma: PrismaService) {} // ✅ inject
+  constructor(private prisma: PrismaService) {}
 
   async create(data: any) {
-    return this.prisma.urunler.create({ data }); // ✅
+    return this.prisma.urunler.create({ data });
+  }
+
+  async findAll() { // ✅ EKLENDİ
+    return this.prisma.urunler.findMany();
   }
 
   async findByBarcode(barkod: string) {
     return this.prisma.urunler.findFirst({
-      where: { barkodu: barkod }, // ⚠️ alan adı senin schema’da "barkodu"
+      where: { barkodu: barkod },
     });
   }
 }
