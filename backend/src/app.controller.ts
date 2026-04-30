4f51d641f8742a8fcebed1f6e2f77e424b7160a8
celalkale-gitcode <celalkale@gmail.com>
2026-04-30 12:52:55 +0300

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Head } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'ERP Backend Çalışıyor! 🚀';
+  @Head() // Hem GET hem HEAD isteklerine cevap ver
+  @HttpCode(200)
+  getHello() {
+    return { status: 'ok', message: 'ERP Backend is running' };
   }
 }
+
 
